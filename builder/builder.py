c7df35c7658a92eb4f82cdfca24b587a946f633c
Michał Jurkiewicz <michaj32@gmail.com>
2020-10-31 15:57:18 +0100

    definition

--- builder/builder.py
+++ builder/builder.py
@@ -1,8 +1,29 @@
-class Builder:
+class House:
     def __init__(self):
+        self._represantation =''
         pass
-
+    def create_walls(self, color):
+        self._represantation+= f"{color} walls"
+    def __repr__(self):
+        return f"house has: {self._represantation}"
 
     @staticmethod
     def build(config):
-        pass
+        house=House()
+
+        return house
+
+config= {'walls': 'white', "backyard":'big'}
+print(config['walls'])
+print(config.keys())
+print(config.items())
+home = House.build(config)
+
+for key in config.keys():
+    print(key, ':', config[key])
+
+for key, value in config.items():
+    print(key, ':', value)
+h = House()
+h.create_walls('white')
+print(h)
